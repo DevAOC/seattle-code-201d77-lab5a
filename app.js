d53fb719fea00ba1c013004cfc0e253ef93f000b
AntoineCharette <antoineocharette@gmail.com>
2021-07-16 18:02:05 -0400

    adds samAndMultiply function

--- app.js
+++ app.js
@@ -54,11 +54,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum = a + b + c;
+  let multiply = a * b * c;
+  let sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.';
+  console.log(sumString);
+  let multiplyString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiply + '.';
+  console.log(multiplyString);
+  return [sum, multiply, sumString, multiplyString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
